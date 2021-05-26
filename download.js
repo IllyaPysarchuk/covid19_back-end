646b1e28e03e597f6076def1cab552e82cd3d921
Illya Alexeevich <flimka134@gmail.com>
2021-05-26 18:10:12 +0300

    Init project.

--- download.js
+++ download.js
@@ -2,7 +2,7 @@ const Fs = require('fs')
 const Path = require('path')
 const Axios = require('axios')
 
-async function downloadImage (url_name) {
+async function downloadFile (url_name) {
     const url = url_name
     const urlname = url_name.slice(63);
     const path = Path.resolve(__dirname, 'files', urlname)
@@ -23,6 +23,6 @@ async function downloadImage (url_name) {
         console.log(urlname + " downloaded")
         })
 }
-downloadImage('https://raw.githubusercontent.com/VasiaPiven/covid19_ua/master/covid19_by_area_type_hosp_dynamics.csv')
-downloadImage('https://raw.githubusercontent.com/VasiaPiven/covid19_ua/master/covid19_by_settlement_actual.csv')
-downloadImage('https://raw.githubusercontent.com/VasiaPiven/covid19_ua/master/covid19_by_settlement_dynamics.zip')
+downloadFile('https://raw.githubusercontent.com/VasiaPiven/covid19_ua/master/covid19_by_area_type_hosp_dynamics.csv')
+downloadFile('https://raw.githubusercontent.com/VasiaPiven/covid19_ua/master/covid19_by_settlement_actual.csv')
+downloadFile('https://raw.githubusercontent.com/VasiaPiven/covid19_ua/master/covid19_by_settlement_dynamics.zip')
